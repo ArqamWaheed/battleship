02bfd30efe3af1dfd050170dbf01f792250ad935
ArqamWaheed <86839084+ArqamWaheed@users.noreply.github.com>
2025-09-27 01:49:52 +0500

    Almost finished the gameboard class with TDD

--- src/index.js
+++ src/index.js
@@ -17,7 +17,7 @@ function Ship(length) {
     return hitsReceived;
   };
 
-  const getLength = function() {
+  const getLength = function () {
     return length;
   };
 
@@ -25,7 +25,7 @@ function Ship(length) {
 }
 
 class Gameboard {
-  constructor() { 
+  constructor() {
     this.aircraft = Ship(5);
     this.battleship = Ship(4);
     this.cruiser = Ship(3);
@@ -33,6 +33,35 @@ class Gameboard {
     this.destroyer2 = Ship(2);
     this.submarine1 = Ship(1);
     this.submarine1 = Ship(1);
+    this.board = this.#initializeGameboard();
+  }
+
+  #initializeGameboard() {
+    const board = [];
+    for (let i = 0; i < 10; i++) {
+      board[i] = Array(10).fill(null);
+    }
+    return board;
+  }
+
+  placeShip(shipName, startingCoord, endingCoord) {
+    const [x1, y1] = startingCoord;
+    const [x2, y2] = endingCoord;
+    if (!this.verifyCoordinates(x1, y1) || !this.verifyCoordinates(x2, y2))
+      throw Error("Invalid coordinates");
+    this.board[x1][y1] = shipName;
+    if (x1 - x2 !== 0) {
+      for (let i = 1; i < Math.abs(x1 - x2) + 1; i++) {
+        this.board[Math.min(x1, x2) + i][y1] = shipName;
+      }
+    }
+    this.board[x2][y2] = shipName;
+  }
+
+  verifyCoordinates(x, y) {
+    // Checks if its out of bound or occupied
+    if (x > 9 || y > 9 || x < 0 || y < 0) return false;
+    return this.board[x][y] === null ? true : false;
   }
 }
 
--- tests/gameboard.test.js
+++ tests/gameboard.test.js
@@ -1,11 +1,32 @@
 import { Gameboard } from "../src";
 
-test("Gameboard ship initializiation test: ", () => {
-    const testGameboard = new Gameboard();
-    expect(testGameboard.aircraft.getLength()).toBe(5);
-    expect(testGameboard.battleship.getLength()).toBe(4);
-    expect(testGameboard.cruiser.getLength()).toBe(3);
-    expect(testGameboard.destroyer1.getLength()).toBe(2);
-    expect(testGameboard.submarine1.getLength()).toBe(1);
+test("Gameboard ships initializiation test: ", () => {
+  const testGameboard = new Gameboard();
+  expect(testGameboard.aircraft.getLength()).toBe(5);
+  expect(testGameboard.battleship.getLength()).toBe(4);
+  expect(testGameboard.cruiser.getLength()).toBe(3);
+  expect(testGameboard.destroyer1.getLength()).toBe(2);
+  expect(testGameboard.submarine1.getLength()).toBe(1);
+});
 
-})
+test("Gameboard board array initialization: ", () => {
+  const testGameboard = new Gameboard();
+  expect(testGameboard.board.length).toBe(10);
+  expect(testGameboard.board[0].length).toBe(10);
+});
+
+test("Gameboard coordinate verification: ", () => {
+  const testGameboard = new Gameboard();
+  expect(testGameboard.verifyCoordinates(10, 11)).toBe(false);
+  expect(testGameboard.verifyCoordinates(-3, 2)).toBe(false);
+  expect(testGameboard.verifyCoordinates(4, 3)).toBe(true);
+});
+
+test("Gameboard Place Ship Verification: ", () => {
+  const testGameboard = new Gameboard();
+  testGameboard.placeShip(testGameboard.cruiser, [0, 3], [2, 3]);
+  expect(testGameboard.board[0][3]).toBe(testGameboard.cruiser);
+  expect(testGameboard.board[1][3]).toBe(testGameboard.cruiser);
+  expect(testGameboard.board[2][3]).toBe(testGameboard.cruiser);
+  expect(testGameboard.board[3][3]).not.toBe(testGameboard.cruiser);
+});
