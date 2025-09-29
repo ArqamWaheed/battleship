db97d41fbb581626dca03d5845fa6155f13689d0
ArqamWaheed <86839084+ArqamWaheed@users.noreply.github.com>
2025-09-30 00:21:49 +0500

    Created receiveAttack function with TDD

--- src/core/models/Gameboard.js
+++ src/core/models/Gameboard.js
@@ -8,7 +8,7 @@ class Gameboard {
     this.destroyer1 = Ship(2);
     this.destroyer2 = Ship(2);
     this.submarine1 = Ship(1);
-    this.submarine1 = Ship(1);
+    this.submarine2 = Ship(1);
     this.board = this.#initializeGameboard();
     this.missedShots = []; // 2D Array containing all the missed shots
   }
@@ -25,7 +25,7 @@ class Gameboard {
     const [x1, y1] = startingCoord;
     const [x2, y2] = endingCoord;
 
-    this.verifyCoordinates(x1, y1); // Error handling
+    this.#verifyCoordinates(x1, y1); // Error handling
     if (
       !this.#verifyOccupiedCoordinates(x1, y1) ||
       !this.#verifyOccupiedCoordinates(x2, y2)
@@ -47,7 +47,7 @@ class Gameboard {
     return;
   }
 
-  verifyCoordinates(x, y) {
+  #verifyCoordinates(x, y) {
     // Checks if its out of bound or occupied
     if (x > 9 || y > 9 || x < 0 || y < 0) throw Error("Out Of Bounds");
   }
@@ -59,7 +59,32 @@ class Gameboard {
 
   receiveAttack(coordinates) {
     const [x1, y1] = coordinates;
-    this.verifyCoordinates(x1, y1);
+    this.#verifyCoordinates(x1, y1);
+
+    if (
+      !this.#verifyOccupiedCoordinates(x1, y1) &&
+      !this.missedShots.includes(coordinates)
+    ) {
+      // Call hit method if ship exists
+      this.board[x1][y1].hit(); // call the hit method on the ship instance.
+    } else if (!this.missedShots.includes(coordinates)) {
+      // Add to misses if the coordinate has not already been struck
+      this.missedShots.push(coordinates);
+    } else {
+      throw Error("Wtf how u genuinely get here"); // Called the method, theres no ship here and it was a miss coordinate which should not be called in the first place
+    }
+  }
+
+  checkAllShipsSunk() {
+    return (
+      this.aircraft.isSunk() &&
+      this.battleship.isSunk() &&
+      this.cruiser.isSunk() &&
+      this.destroyer1.isSunk() &&
+      this.destroyer2.isSunk() &&
+      this.submarine1.isSunk() &&
+      this.submarine2.isSunk()
+    ); // Note: you had submarine1 twice, should be submarine2
   }
 }
 
--- tests/gameboard.test.js
+++ tests/gameboard.test.js
@@ -17,9 +17,51 @@ test("Gameboard board array initialization: ", () => {
 
 test("Gameboard Place Ship Verification: ", () => {
   const testGameboard = new Gameboard();
+  expect(testGameboard.board[0][3]).toBe(null);
+  expect(testGameboard.board[1][3]).toBe(null);
+  expect(testGameboard.board[2][3]).toBe(null);
   testGameboard.placeShip(testGameboard.cruiser, [0, 3], [2, 3]);
   expect(testGameboard.board[0][3]).toBe(testGameboard.cruiser);
   expect(testGameboard.board[1][3]).toBe(testGameboard.cruiser);
   expect(testGameboard.board[2][3]).toBe(testGameboard.cruiser);
   expect(testGameboard.board[3][3]).not.toBe(testGameboard.cruiser);
+});
+
+test("Gameboard receiveAttack functionality", () => {
+  const testGameboard = new Gameboard();
+  const coordinates = [0, 3];
+  const coordinatesMiss = [3, 5];
+  testGameboard.placeShip(testGameboard.cruiser, coordinates, [2, 3]);
+  expect(testGameboard.cruiser.getHitsReceived()).toBe(0);
+  testGameboard.receiveAttack(coordinates);
+  expect(testGameboard.cruiser.getHitsReceived()).toBe(1);
+  expect(testGameboard.missedShots).toEqual([]);
+  testGameboard.receiveAttack(coordinatesMiss);
+  expect(testGameboard.missedShots).toEqual([coordinatesMiss]);
+});
+
+test("Gameboard all sunk functions, tests ship hit functionality aswell.", () => {
+  const testGameboard = new Gameboard();
+  expect(testGameboard.checkAllShipsSunk()).toBe(false);
+
+  for (let i = 0; i < 5; i++) {
+    testGameboard.aircraft.hit();
+  }
+  for (let i = 0; i < 4; i++) {
+    testGameboard.battleship.hit();
+  }
+  for (let i = 0; i < 3; i++) {
+    testGameboard.cruiser.hit();
+  }
+  for (let i = 0; i < 2; i++) {
+    testGameboard.destroyer1.hit();
+  }
+  for (let i = 0; i < 2; i++) {
+    testGameboard.destroyer2.hit();
+  }
+
+  testGameboard.submarine1.hit();
+  expect(testGameboard.checkAllShipsSunk()).toBe(false);
+  testGameboard.submarine2.hit();
+  expect(testGameboard.checkAllShipsSunk()).toBe(true);
 });
